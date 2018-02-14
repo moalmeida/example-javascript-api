47ac5b28ecabf303119626ab70d3b6e1c0ac2101
MAURO ALMEIDA <moalmeida@koinosystems.com>
2018-02-14 02:09:23 -0200

    .

--- erase_data.sh
+++ erase_data.sh
@@ -4,3 +4,8 @@ docker-compose down --remove-orphans
 for i in $(docker volume ls |grep examplejavascriptapi|awk '{print $2;}');do docker volume rm $i -f;done
 for i in $(docker ps -a |grep examplejavascriptapi|awk '{print $1;}');do docker rm $i -f;done
 for i in $(docker images |grep examplejavascriptapi|awk '{print $3;}');do docker rmi $i -f;done
+
+if [ "$(docker ps -q -f name=traefik)" ]; then
+  echo "erase ops dependencies"
+  curl https://raw.githubusercontent.com/moalmeida/example-ops/master/erase.sh | bash
+fi
--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-docker network create frontend
-docker network create backend
+if [ ! "$(docker ps -q -f name=traefik)" ]; then
+  echo "run ops dependencies"
+  curl https://raw.githubusercontent.com/moalmeida/example-ops/master/startup.sh | bash
+fi
+
 docker-compose down --remove-orphans
 for i in $(docker ps -a |grep examplejavascriptapi|awk '{print $1;}');do docker rm $i -f;done
 for i in $(docker images |grep examplejavascriptapi|awk '{print $3;}');do docker rmi $i -f;done
